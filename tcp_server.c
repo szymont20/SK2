6e743b8d816344dd00accba58c499f1ff5971e8c
szymont20 <147140577+szymont20@users.noreply.github.com>
2024-12-03 15:31:48 +0100

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -11,9 +11,11 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr, client_addr;
     char buffer[256];
     int PORT = atoi(argv[1]);
+    int on = 1;
 
     //Tworzenie gniazda TCP
     server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
 
     //Ustawianie adresu serwera
     server_addr.sin_family = AF_INET;
@@ -30,7 +32,7 @@ int main(int argc, char *argv[]) {
     //Akceptowanie połączenia od klienta
     socklen_t client_len = sizeof(client_addr);
     client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-    printf("Połączenie klientem: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    printf("Połączenie z klientem: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
     //Odbieranie danych od klienta
     read(client_fd, buffer, 256);
